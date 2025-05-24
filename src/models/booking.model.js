148e4c165f26f893ddfdbe0c14535fc6eaa529db
khaledBakry99 <114497285+khaledBakry99@users.noreply.github.com>
2025-05-24 15:47:13 +0300

    Add files via upload

--- src/models/booking.model.js
+++ src/models/booking.model.js
@@ -37,7 +37,7 @@ const bookingSchema = new mongoose.Schema(
     },
     status: {
       type: String,
-      enum: ['pending', 'accepted', 'rejected', 'completed', 'cancelled'],
+      enum: ['pending', 'accepted', 'rejected', 'completed', 'cancelled', 'cancelled_expired'],
       default: 'pending',
     },
     price: {
