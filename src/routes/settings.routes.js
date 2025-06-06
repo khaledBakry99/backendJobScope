0f1958fe5ecb6642ba8d8c283fb30fb89520ce89
khaledBakry99 <114497285+khaledBakry99@users.noreply.github.com>
2025-06-07 02:36:16 +0300

    Add files via upload

--- src/routes/settings.routes.js
+++ src/routes/settings.routes.js
@@ -1,8 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getSettings, updateSettings } = require('../controllers/settings.controller');
-const { protect, admin } = require('../middleware/auth.middleware');
+const {
+  getSettings,
+  updateSettings,
+} = require("../controllers/settings.controller");
+const { protect, admin } = require("../middleware/auth.middleware");
 
-router.route('/').get(getSettings).put(protect, admin, updateSettings);
+router.get("/", getSettings);
+router.put("/", protect, admin, updateSettings);
 
 module.exports = router;
